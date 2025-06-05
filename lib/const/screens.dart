13a61ab2fb926be8803db25c44b8745b45938010
김광회 <elian118@naver.com>
2025-06-05 13:01:01 +0900

    apply screens data

---
+++ lib/const/screens.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_animations/screens/apple_watch_screen.dart';
+import 'package:flutter_animations/screens/color_tween_screen.dart';
+import 'package:flutter_animations/screens/combine_explicit_animations_screen.dart';
+import 'package:flutter_animations/screens/container_transform_screen.dart';
+import 'package:flutter_animations/screens/curved_animation_screen.dart';
+import 'package:flutter_animations/screens/explicit_animation_screen.dart';
+import 'package:flutter_animations/screens/implicit_animation_screen.dart';
+import 'package:flutter_animations/screens/music_player_screen.dart';
+import 'package:flutter_animations/screens/rive_custom_balls_screen.dart';
+import 'package:flutter_animations/screens/rive_custom_btn_screen.dart';
+import 'package:flutter_animations/screens/rive_dwarf_screen.dart';
+import 'package:flutter_animations/screens/rive_stars_screen.dart';
+import 'package:flutter_animations/screens/shared_axis_screen.dart';
+import 'package:flutter_animations/screens/swiping_cards_screen.dart';
+import 'package:flutter_animations/screens/value_notifier_screen.dart';
+
+List<Map<String, dynamic>> screens = [
+  {"title": "Implicit animation", "widget": ImplicitAnimationScreen()},
+  {"title": "Explicit animation", "widget": ExplicitAnimationScreen()},
+  {"title": "Color Tween animation", "widget": ColorTweenScreen()},
+  {
+    "title": "Combine explicit animations",
+    "widget": DecoratedBoxTransitionScreen(),
+  },
+  {"title": "Curved animation", "widget": CurvedAnimationScreen()},
+  {"title": "with Value notifier", "widget": ValueNotifierScreen()},
+  {"title": "Apple Watch", "widget": AppleWatchScreen()},
+  {"title": "Swiping Cards", "widget": SwipingCardsScreen()},
+  {"title": "Music Player", "widget": MusicPlayerScreen()},
+  {"title": "Rive - Dwarf", "widget": RiveDwarfScreen()},
+  {"title": "Rive - Stars", "widget": RiveStarsScreen()},
+  {"title": "Rive - Custom Balls", "widget": RiveCustomBallsScreen()},
+  {"title": "Rive - Custom Button", "widget": RiveCustomBtnScreen()},
+  {"title": "Container transform", "widget": ContainerTransformScreen()},
+  {"title": "Shared Axis", "widget": SharedAxisScreen()},
+];
--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/screens/apple_watch_screen.dart';
-import 'package:flutter_animations/screens/color_tween_screen.dart';
-import 'package:flutter_animations/screens/container_transform_screen.dart';
-import 'package:flutter_animations/screens/curved_animation_screen.dart';
-import 'package:flutter_animations/screens/implicit_animation_screen.dart';
-import 'package:flutter_animations/screens/music_player_screen.dart';
-import 'package:flutter_animations/screens/rive_custom_balls_screen.dart';
-import 'package:flutter_animations/screens/rive_custom_btn_screen.dart';
-import 'package:flutter_animations/screens/rive_dwarf_screen.dart';
-import 'package:flutter_animations/screens/rive_stars_screen.dart';
-import 'package:flutter_animations/screens/shared_axis_screen.dart';
-import 'package:flutter_animations/screens/swiping_cards_screen.dart';
-import 'package:flutter_animations/screens/value_notifier_screen.dart';
-
-import 'combine_explicit_animations_screen.dart';
-import 'explicit_animation_screen.dart';
+import 'package:flutter_animations/const/screens.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -27,86 +12,17 @@ class MenuScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Fluter Animations')),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () => _goToPage(context, ImplicitAnimationScreen()),
-                child: Text('Implicit animation'),
+        child: ListView.separated(
+          itemBuilder:
+              (context, index) => Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () => _goToPage(context, screens[index]['widget']),
+                  child: Text(screens[index]['title']),
+                ),
               ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, ExplicitAnimationScreen()),
-                child: Text('Explicit animation'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, ColorTweenScreen()),
-                child: Text('Color Tween animation'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed:
-                    () => _goToPage(context, DecoratedBoxTransitionScreen()),
-                child: Text('Combine explicit animations'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, CurvedAnimationScreen()),
-                child: Text('Curved animation'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, ValueNotifierScreen()),
-                child: Text('with Value notifier'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, AppleWatchScreen()),
-                child: Text('Apple Watch'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, SwipingCardsScreen()),
-                child: Text('Swiping Cards'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, MusicPlayerScreen()),
-                child: Text('Music Player'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, RiveDwarfScreen()),
-                child: Text('Rive - Dwarf'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, RiveStarsScreen()),
-                child: Text('Rive - Stars'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, RiveCustomBallsScreen()),
-                child: Text('Rive - Custom Balls'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, RiveCustomBtnScreen()),
-                child: Text('Rive - Custom Button'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, ContainerTransformScreen()),
-                child: Text('Container transform'),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () => _goToPage(context, SharedAxisScreen()),
-                child: Text('Shared Axis'),
-              ),
-            ],
-          ),
+          separatorBuilder: (context, index) => SizedBox(height: 10),
+          itemCount: 15,
         ),
       ),
     );

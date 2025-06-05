299572c2446a4864dd5d48136e413a4140e28b38
김광회 <elian118@naver.com>
2025-06-05 16:45:15 +0900

    #8.4 AnimateList

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_animations/const/credit_cards.dart';
 import 'package:flutter_animations/widgets/comps/credit_card.dart';
 
@@ -17,15 +18,37 @@ class _WalletScreenState extends State<WalletScreen> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          // 메서드 체이닝 방식
           children: [
-            ...creditCard.map(
-              (card) => CreditCard(
-                bgColor: card['bgColor'],
-                username: card['username'],
-                number: card['number'],
-              ),
-            ),
-          ],
+                ...creditCard.map(
+                  (card) => CreditCard(
+                    bgColor: card['bgColor'],
+                    username: card['username'],
+                    number: card['number'],
+                  ),
+                ),
+              ]
+              .animate(interval: 0.2.seconds)
+              .slideX(begin: -1, end: 0)
+              .fadeIn(begin: 0),
+
+          // 코드를 더 명확화한 방식
+          // children: [AnimateList(
+          //   effects: [
+          //     SlideEffect(begin: Offset(-1, 0), end: Offset.zero),
+          //     FadeEffect(begin: 0, end: 1),
+          //   ],
+          //   interval: 0.2.seconds, // 200.ms
+          //   children: [
+          //     ...creditCard.map(
+          //       (card) => CreditCard(
+          //         bgColor: card['bgColor'],
+          //         username: card['username'],
+          //         number: card['number'],
+          //       ),
+          //     ),
+          //   ],
+          // )],
         ),
       ),
     );
